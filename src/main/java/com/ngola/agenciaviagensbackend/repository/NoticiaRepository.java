a783d73436ad8ec9211f8cdf4408acc29b7f1da3
Antonio ngola <antoniongola10@gmail.com>
2024-08-15 13:18:12 +0100

    adding the app repositories

---
+++ src/main/java/com/ngola/agenciaviagensbackend/repository/NoticiaRepository.java
@@ -0,0 +1,7 @@
+package com.ngola.agenciaviagensbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticiaRepository extends JpaRepository<NoticiaRepository, Integer> {
+
+}
