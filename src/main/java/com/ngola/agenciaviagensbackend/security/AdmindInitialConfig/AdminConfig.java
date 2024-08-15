c4cb5528619e31fdb7efe3bf016e19f388543405
Antonio ngola <antoniongola10@gmail.com>
2024-08-15 11:59:00 +0100

    adding the initial security configs

---
+++ src/main/java/com/ngola/agenciaviagensbackend/security/AdmindInitialConfig/AdminConfig.java
@@ -0,0 +1,15 @@
+package com.ngola.agenciaviagensbackend.security.AdmindInitialConfig;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdminConfig implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
